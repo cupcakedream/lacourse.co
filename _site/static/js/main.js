d7e1ed48ed2093ad66b8a9d5c8bba970478c5302
Mike Lacourse <mike@Matchbook.local>
2015-11-23 16:08:27 -0500

    Style Changes

--- _site/static/js/main.js
+++ _site/static/js/main.js
@@ -63,26 +63,32 @@ $("#make-request").submit(function(event){
     // Let's select and cache all the fields
     var $inputs = $form.find("input, select, textarea");
     
-    
-    
     if($inputs[0].value == '') {
     	$($inputs[0]).addClass('error');
     	$($inputs[0]).prev().addClass('error');
+		dest = $('#make-request').offset().top - 100;
+		$('html,body').animate({scrollTop: dest}, 350, 'swing');		
     	return false;
     }
     else if($inputs[1].value == '') {
     	$($inputs[1]).addClass('error');
     	$($inputs[1]).prev().addClass('error');
+		dest = $('#make-request').offset().top - 100;
+		$('html,body').animate({scrollTop: dest}, 350, 'swing');
     	return false;
     }
     else if($inputs[2].value == '') {
     	$($inputs[2]).addClass('error');
     	$($inputs[2]).prev().addClass('error');
+		dest = $('#make-request').offset().top - 100;
+		$('html,body').animate({scrollTop: dest}, 350, 'swing');
     	return false;
     }
     else if($inputs[4].value == '') {
     	$($inputs[4]).addClass('error');
     	$($inputs[4]).prev().addClass('error');
+		dest = $('#make-request').offset().top - 100;
+		$('html,body').animate({scrollTop: dest}, 350, 'swing');
     	return false;
     }
 
@@ -105,6 +111,7 @@ $("#make-request").submit(function(event){
     request.done(function (response, textStatus, jqXHR){
         // Log a message to the console
         $form.append("<p style='font-size:17px;line-height:28px;font-weight:400;'>Your request has been sent, thanks! I'll review your project requirements and get back to you as soon as possible.</p>");
+   		dest = $(document).height() - $(window).height();
    		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
     });
 
@@ -112,8 +119,8 @@ $("#make-request").submit(function(event){
     request.fail(function (jqXHR, textStatus, errorThrown){
         // Log the error to the console
         $form.append("<p style='font-size:17px;line-height:28px;font-weight:400;'>There was a problem processing your request, please reload the page and try again or contact me directly at mike@lacourse.co</p>");
+        dest = $(document).height() - $(window).height();
         $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-        $inputs.prop("disabled", false);
     });
 
     // Prevent default posting of form
@@ -135,54 +142,10 @@ $(document).ready(function() {
 	  	return height_offset;
 	  }
 	});
-
-	// 2nd waypoint transitions sidebar from "learn more" to "request a quote" section
-// 	window.waypoint_2 = $('#learn-more').waypoint({
-// 	  handler: function(direction) {
-// 		$('#side-history .stick').toggleClass('end');
-// 	  },
-// 	  continuous: false,
-// 	  offset: function() { 
-// 	    height_offset = -1*($('#learn-more').outerHeight() - $('#side-history .stick').outerHeight());
-// 	  	return height_offset;
-// 	  }
-// 	});
 	
 	var side_stick_1 = new Waypoint.Sticky({
 	  element: $('#side-about .stick')[0]
 	});
-// 	var side_stick_2 = new Waypoint.Sticky({
-// 	  element: $('#side-history .stick')[0]
-// 	});
-// 	var side_stick_2 = new Waypoint.Sticky({
-// 	  element: $('#side-reviews .stick')[0]
-// 	});
-	
-// 	var waypoint2 = $('#learn-more').waypoint({
-// 	  handler: function(direction) {
-// 	  	if (direction == 'down') {
-// 			update_sidebar(direction,2);
-// 		}
-// 	  },
-// 	  offset: 500,
-// 	  continuous: false
-// 	});
-// 	var waypoint3 = $('#learn-more').waypoint({
-// 	  handler: function(direction) {
-// 	  	if (direction == 'up') {
-// 			update_sidebar(direction,2);
-// 		}
-// 	  },
-// 	  offset: 'bottom-in-view',
-// 	  continuous: false
-// 	});
-// 	var waypoint4 = $('#request-a-quote').waypoint({
-// 	  handler: function(direction) {
-// 		update_sidebar(direction,3);
-// 	  },
-// 	  continuous: false,
-// 	  offset: 50
-// 	});
 
 // Gallery for Adventures
 	$('.adventures').magnificPopup({
@@ -209,112 +172,5 @@ $(document).ready(function() {
 		}
 		e.preventDefault();
 	});
-
-	// Drop Downs
-// 	$('.drop-toggle').click(function(e) {
-// 		$element = $(this);
-// 		if ( !$element.parent().hasClass('open') ) {
-// 			$('.drop-text, .drop-title').removeClass('open');
-// 			$element.parent().addClass('open');
-// 			$('.drop-text').slideUp(200);
-// 			$element.parent().next().slideDown('fast', function() {				
-// 				$element.parent().next().addClass('open');
-// 				window.waypoint_2.triggerPoint = -1*($('#learn-more').outerHeight() - $('#side-history .stick').outerHeight());
-// 			});
-// 		}
-// // 		else {	
-// // 			$element.parent().removeClass('open');
-// // 			$element.parent().next().slideUp(200, function() {
-// // 				$element.parent().next().removeClass('open');
-// // 				window.waypoint_2.triggerPoint = -1*($('#learn-more').outerHeight() - $('#side-history .stick').outerHeight());
-// // 			});	
-// // 		}
-// 		e.preventDefault();
-// 	});
-	
 });
 
-// $(document).ready(function() {
-// 	var waypoint1 = $('#portfolio').waypoint({
-// 	  handler: function(direction) {
-// 		//update_sidebar(direction,1);
-// 	  },
-// 	  continuous: false,
-// 	  offset: 'bottom-in-view',
-// 	});
-// 	var waypoint2 = $('#learn-more').waypoint({
-// 	  handler: function(direction) {
-// 	  	if (direction == 'down') {
-// 			update_sidebar(direction,2);
-// 		}
-// 	  },
-// 	  offset: 500,
-// 	  continuous: false
-// 	});
-// 	var waypoint3 = $('#learn-more').waypoint({
-// 	  handler: function(direction) {
-// 	  	if (direction == 'up') {
-// 			update_sidebar(direction,2);
-// 		}
-// 	  },
-// 	  offset: 'bottom-in-view',
-// 	  continuous: false
-// 	});
-// 	var waypoint4 = $('#request-a-quote').waypoint({
-// 	  handler: function(direction) {
-// 		update_sidebar(direction,3);
-// 	  },
-// 	  continuous: false,
-// 	  offset: 50
-// 	});
-// });
-
-// Subtle background parallax effect
-
-var lastScrollY = 0,
-    ticking = false,
-    bgElm = document.getElementById('background'),
-    speedDivider = 3;
-
-// Update scroll value and request tick
-var doScroll = function() {
-  lastScrollY = window.pageYOffset;
-  requestTick();
-};
-
-window.addEventListener('scroll', doScroll, false);
-
-var requestTick = function() {
-  if (!ticking) {
-    window.requestAnimationFrame(updatePosition);
-    ticking = true;
-  }
-};
-
-var updatePosition = function() {
-  var translateValue = lastScrollY / speedDivider;
-
-  // We don't want parallax to happen if scrollpos is below 0
-  if (translateValue < 0)
-    translateValue = 0;
-
-  translateY3d(bgElm, translateValue);
-
-  // Stop ticking
-  ticking = false;
-};
-
-// Translates an element on the Y axis using translate3d
-// to ensure that the rendering is done by the GPU
-var translateY3d = function(elm, value) {
-//   var translate = 'translate3d(0px,' + value + 'px, 0px)';
-//   elm.style['-webkit-transform'] = translate;
-//   elm.style['-moz-transform'] = translate;
-//   elm.style['-ms-transform'] = translate;
-//   elm.style['-o-transform'] = translate;
-//   elm.style.transform = translate;
-  
-  value = value * .6;
-  //elm.style['background-position'] = '0px -' + value + 'px';
-  
-};
